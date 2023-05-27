2414035b2ced0d14264cea7da0d4d2b8089e6717
ilyes <elyes.sellami99@gmail.com>
2023-05-27 12:41:49 +0100

    adding GMC-PROJECT

---
+++ script.js
@@ -0,0 +1,17 @@
+var counterVal = 0;
+
+function incrementClick() {
+    updateDisplay(++counterVal);
+}
+
+function resetCounter() {
+    counterVal = 0;
+    updateDisplay(counterVal);
+}
+
+function updateDisplay(val) {
+    document.getElementById("counter-label").innerHTML = val;
+}
+
+
+
